bbef392f4fa23b4de3d98ade89838ac0d1c65897
caiogmrocha <caioguilherme739@gmail.com>
2024-10-18 22:11:21 -0300

    feat: implements sendMessage request type to websocket server

--- server/cmd/websockets/main.go
+++ server/cmd/websockets/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/olahol/melody"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	m := melody.New()
+
+	http.HandleFunc("/ws", func (w http.ResponseWriter, r *http.Request) {
+		m.HandleRequest(w, r)
+	})
+
+	// users := map[string]string{}
+	chats := map[string][]string{}
+
+	m.HandleMessage(func (s *melody.Session, msg []byte) {
+		userId := s.Request.Header.Get("User-ID")
+		
+		var clientPayload map[string]interface{}
+
+		json.Unmarshal(msg, &clientPayload)
+		
+		chatKey := fmt.Sprintf("%s-%s", userId, clientPayload["receiver_id"])
+
+		s.Set(chatKey, true)
+
+		switch clientPayload["type"] {
+			case "sendMessage": {
+				sessions, err := m.Sessions()
+
+				if err != nil {
+					fmt.Println(err)
+
+					return
+				}
+
+				chats[chatKey] = append(chats[chatKey], clientPayload["message"].(string))
+
+				for _, session := range sessions {
+					if session.Request.Header.Get("User-ID") == clientPayload["receiver_id"] {
+						responsePayload := map[string]interface{}{
+							"type": "message",
+							"sender_id": userId,
+							"message": clientPayload["message"],
+						}
+
+						marshalledPayload, _ := json.Marshal(responsePayload)
+
+						session.Write(marshalledPayload)
+					}
+				}
+			}
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
