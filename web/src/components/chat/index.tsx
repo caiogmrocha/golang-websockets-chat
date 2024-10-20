7ac9ef40813acd0eb3f11af445ceecf98e2224cf
caiogmrocha <caioguilherme739@gmail.com>
2024-10-19 21:12:11 -0300

    feat(web): add .editorconfig

--- web/src/components/chat/index.tsx
+++ web/src/components/chat/index.tsx
@@ -1,166 +1,166 @@
 import { Button } from "@/components/ui/button"
 import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Message } from "./message"
 import { useEffect, useState } from "react"
 
 interface Message {
-    owner: "sender" | "receiver"
-    message: string
+  owner: "sender" | "receiver"
+  message: string
 }
 
 export function Chat() {
-    const [messages, setMessages] = useState<Message[]>([
-        { owner: "receiver", message: "Hi, how can I help you today?" },
-        { owner: "sender", message: "What seems to be the problem?" },
-        { owner: "receiver", message: "Hey, I'm having trouble with my account." },
-        { owner: "sender", message: "I can't log in." },
-    ]);
+  const [messages, setMessages] = useState<Message[]>([
+    { owner: "receiver", message: "Hi, how can I help you today?" },
+    { owner: "sender", message: "What seems to be the problem?" },
+    { owner: "receiver", message: "Hey, I'm having trouble with my account." },
+    { owner: "sender", message: "I can't log in." },
+  ]);
 
-    const [currentMessage, setCurrentMessage] = useState("")
-    
-    const [userId, setUserId] = useState("")
-    const [usersIds, setUsersIds] = useState<string[]>([])
+  const [currentMessage, setCurrentMessage] = useState("")
 
-    const [webSocket, setWebSocket] = useState<WebSocket | null>(null)
+  const [userId, setUserId] = useState("")
+  const [usersIds, setUsersIds] = useState<string[]>([])
 
-    useEffect(() => {
-        let webSocket = new WebSocket("ws://localhost:8080/ws")
+  const [webSocket, setWebSocket] = useState<WebSocket | null>(null)
 
-        setWebSocket(webSocket)
+  useEffect(() => {
+    let webSocket = new WebSocket("ws://localhost:8080/ws")
 
-        function handleWebSocketOpen() {
-            console.log("Connected to the server")
+    setWebSocket(webSocket)
 
-            webSocket.send(JSON.stringify({
-                type: "users_ids",
-            }))
-        }
+    function handleWebSocketOpen() {
+      console.log("Connected to the server")
 
-        function handleWebSocketMessage(event: MessageEvent) {
-            console.log("Message from the server:", event.data)
-
-            const data = JSON.parse(event.data)
-
-            switch (data.type) {
-                case "message": {
-                    if (data.type === "message") {
-                        setMessages((prevMessages) => [
-                            ...prevMessages,
-                            { owner: "receiver", message: data.message },
-                        ])
-                    }
-                } break;
-
-                case "user_id": {
-                    console.log("User ID:", data.user_id)
-
-                    setUserId(data.user_id)
-                } break;
-
-                case "users_ids": {
-                    console.log("User IDs:", data.users_ids)
-
-                    setUsersIds(data.users_ids)
-                } break;
-
-                case "another_user_connected": {
-                    console.log("Another user connected:", data.user_id)
-
-                    setUsersIds((prevUsersIds) => [
-                        ...prevUsersIds,
-                        data.user_id,
-                    ])
-                } break;
-
-                case "another_user_disconnected": {
-                    console.log("Another user disconnected:", data.user_id)
-
-                    setUsersIds((prevUsersIds) => prevUsersIds.filter((userId) => userId !== data.user_id))
-                } break;
-
-                default: {
-                    console.error("Unknown message type:", data.type)
-                }
-            }
-        }
-
-        function handleWebSocketClose() {
-            console.log("Disconnected from the server")
-        }
-
-        function handleWebSocketError(error: Event) {
-            console.error("An error occurred:", error)
-        }
-
-        webSocket.addEventListener("open", handleWebSocketOpen)
-        webSocket.addEventListener("message", handleWebSocketMessage)
-        webSocket.addEventListener("close", handleWebSocketClose)
-        webSocket.addEventListener("error", handleWebSocketError)
-
-        return () => {
-            webSocket.removeEventListener("open", handleWebSocketOpen)
-            webSocket.removeEventListener("message", handleWebSocketMessage)
-            webSocket.removeEventListener("close", handleWebSocketClose)
-            webSocket.removeEventListener("error", handleWebSocketError)
-            webSocket.close()
-        }
-    }, [])
-
-    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault()
-
-        setMessages((prevMessages) => [
-            ...prevMessages,
-            { owner: "sender", message: currentMessage },
-        ])
-
-        setCurrentMessage("")
-
-        webSocket!.send(JSON.stringify({
-            type: "message",
-            message: currentMessage,
-            sender_id: userId,
-            receiver_id: usersIds.find((uid) => uid !== userId),
-        }))
+      webSocket.send(JSON.stringify({
+        type: "users_ids",
+      }))
     }
 
-    function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
-        if (e.key === "Enter") {
-            e.preventDefault()
+    function handleWebSocketMessage(event: MessageEvent) {
+      console.log("Message from the server:", event.data)
+
+      const data = JSON.parse(event.data)
+
+      switch (data.type) {
+        case "message": {
+          if (data.type === "message") {
+            setMessages((prevMessages) => [
+              ...prevMessages,
+              { owner: "receiver", message: data.message },
+            ])
+          }
+        } break;
+
+        case "user_id": {
+          console.log("User ID:", data.user_id)
+
+          setUserId(data.user_id)
+        } break;
+
+        case "users_ids": {
+          console.log("User IDs:", data.users_ids)
+
+          setUsersIds(data.users_ids)
+        } break;
+
+        case "another_user_connected": {
+          console.log("Another user connected:", data.user_id)
+
+          setUsersIds((prevUsersIds) => [
+            ...prevUsersIds,
+            data.user_id,
+          ])
+        } break;
+
+        case "another_user_disconnected": {
+          console.log("Another user disconnected:", data.user_id)
+
+          setUsersIds((prevUsersIds) => prevUsersIds.filter((userId) => userId !== data.user_id))
+        } break;
+
+        default: {
+          console.error("Unknown message type:", data.type)
         }
+      }
     }
 
-    return (
-        <Card className="w-[350px]">
-            <CardHeader>
-                <CardTitle>Chat</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <div className="space-y-4">
-                    {messages.map((message, index) => (
-                        <Message key={index} owner={message.owner}>
-                            {message.message}
-                        </Message>
-                    ))}
-                </div>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-                <form onSubmit={handleSubmit}>
-                    <div className="grid w-full items-center gap-4">
-                        <div className="flex">
-                            <Input id="name" placeholder="Name of your project" onKeyDown={handleKeyDown} onChange={(e) => setCurrentMessage(e.target.value)} value={currentMessage} />
-                            <Button type="submit">Send</Button>
-                        </div>
-                    </div>
-                </form>
-            </CardFooter>
-        </Card>
-    )
+    function handleWebSocketClose() {
+      console.log("Disconnected from the server")
+    }
+
+    function handleWebSocketError(error: Event) {
+      console.error("An error occurred:", error)
+    }
+
+    webSocket.addEventListener("open", handleWebSocketOpen)
+    webSocket.addEventListener("message", handleWebSocketMessage)
+    webSocket.addEventListener("close", handleWebSocketClose)
+    webSocket.addEventListener("error", handleWebSocketError)
+
+    return () => {
+      webSocket.removeEventListener("open", handleWebSocketOpen)
+      webSocket.removeEventListener("message", handleWebSocketMessage)
+      webSocket.removeEventListener("close", handleWebSocketClose)
+      webSocket.removeEventListener("error", handleWebSocketError)
+      webSocket.close()
+    }
+  }, [])
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { owner: "sender", message: currentMessage },
+    ])
+
+    setCurrentMessage("")
+
+    webSocket!.send(JSON.stringify({
+      type: "message",
+      message: currentMessage,
+      sender_id: userId,
+      receiver_id: usersIds.find((uid) => uid !== userId),
+    }))
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {
+      e.preventDefault()
+    }
+  }
+
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Chat</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-4">
+          {messages.map((message, index) => (
+            <Message key={index} owner={message.owner}>
+              {message.message}
+            </Message>
+          ))}
+        </div>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <form onSubmit={handleSubmit}>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex">
+              <Input id="name" placeholder="Name of your project" onKeyDown={handleKeyDown} onChange={(e) => setCurrentMessage(e.target.value)} value={currentMessage} />
+              <Button type="submit">Send</Button>
+            </div>
+          </div>
+        </form>
+      </CardFooter>
+    </Card>
+  )
 }
