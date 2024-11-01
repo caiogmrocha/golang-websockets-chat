8329820c94311c78cc7a18c9fb190a0a64b66ed7
caiogmrocha <caioguilherme739@gmail.com>
2024-10-31 22:09:31 -0300

    feat(server): create HTTP GET /health

--- server/internal/presentation/http/routes/routes.go
+++ server/internal/presentation/http/routes/routes.go
@@ -1,11 +1,19 @@
 package http_routes
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/olahol/melody"
 )
 
 func SetRoutes(router *mux.Router, m *melody.Melody) {
+	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("{\"status\": \"ok\"}"))
+	}).Methods("GET")
+
 	SetUsersRoutes(router)
 	SetWSRoutes(router, m)
 }
