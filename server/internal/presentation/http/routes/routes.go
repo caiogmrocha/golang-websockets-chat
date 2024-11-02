0a0f50dc37e7008cbe80351fdc25a1e7ab68e08a
caiogmrocha <caioguilherme739@gmail.com>
2024-11-02 00:10:43 -0300

    build: configure nginx server

--- server/internal/presentation/http/routes/routes.go
+++ server/internal/presentation/http/routes/routes.go
@@ -1,6 +1,7 @@
 package http_routes
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,7 +11,11 @@ import (
 func SetRoutes(router *mux.Router, m *melody.Melody) {
 	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("{\"status\": \"ok\"}"))
+		w.Header().Set("Content-Type", "application/json")
+
+		responseBody, _ := json.Marshal(map[string]string{"status": "ok"})
+
+		w.Write(responseBody)
 	}).Methods("GET")
 
 	SetUsersRoutes(router)
